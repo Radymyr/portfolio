8243b76becf82bf94850f80ebfb28fca42894721
Radon4ik <92453183+Radon4ik@users.noreply.github.com>
2022-06-30 23:31:07 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './index.scss';
+
+async function check() {
+    await fetch('https://google.com');
+
+}
+
+check().then(() => {
+    console.log("success");
+}).catch(() => {
+    console.log('error');
+})
