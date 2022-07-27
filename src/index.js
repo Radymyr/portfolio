759337c2baa631a7d81968e98867940ff1a38f03
Radon4ik <Loboda.Radimir@gmail.com>
2022-07-27 16:39:02 +0300

    added scss files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import './index.scss';
 
-async function check() {
-    await fetch('https://google.com');
+const burger = document.querySelector('.header__burger');
+const menu = document.querySelector('.hero__menu');
+const headerLine = document.querySelector('.header__line')
+const body = document.querySelector('body')
 
-}
+burger.addEventListener('click', () => {
+  return (burger.classList.toggle('active'),
+    menu.classList.toggle('active'),
+    headerLine.classList.toggle('active'),
+    body.classList.toggle('lock'));
+});
 
-check().then(() => {
-    console.log("success");
-}).catch(() => {
-    console.log('error');
-})
+
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,56 @@
-const { resolve, join } = require('path');
+const {resolve, join} = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: resolve(__dirname, 'build'),
-        filename: '[name]-[fullhash].js',
-        clean: true,
-    },
-    module: {
-        rules: [
-          {
-            test: /\.m?js$/,
-            exclude: /node_modules/,
-            use: {
-              loader: "babel-loader",
-              options: {
-                presets: ['@babel/preset-env'],
-                plugins: ["@babel/plugin-transform-runtime"]
-              }
-            }
-          },
-          {
-            test: /\.s[ac]ss$/i,
-            use: [
-              MiniCssExtractPlugin.loader,
-              "css-loader",
-              "sass-loader",
-            ],
-          },
-        ]
-      },
+  entry: './src/index.js',
+  output: {
+    path: resolve(__dirname, 'build'),
+    filename: '[name]-[fullhash].js',
+    clean: true
 
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
-
-        new MiniCssExtractPlugin({
-            filename: '[name]-[fullhash].css'
-        })
-    ],
-
-    devServer: {
-        port: 8080,
-        static: {
-            directory: join(__dirname, 'src')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ["@babel/plugin-transform-runtime"]
+          }
         }
-    },
-    
-    devtool: 'inline-source-map',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: '[name]-[fullhash].css'
+    })
+  ],
+
+  devServer: {
+    port: 8080,
+    static: {
+      directory: join(__dirname, 'src')
+    }
+  },
+
+  devtool: 'inline-source-map',
 };
 
