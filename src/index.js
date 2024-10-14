e7e52ca512bd2f9fff8dd5d175c91419ce5c9270
Radymyr <loboda.radymyr@gmail.com>
2024-10-14 14:54:10 +0300

    images fixed

--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import './index.scss';
 
 const burger = document.querySelector('.header__burger');
 const menu = document.querySelector('.hero__menu');
-const headerLine = document.querySelector('.header__line')
-const body = document.querySelector('body')
+const headerLine = document.querySelector('.header__line');
+const body = document.querySelector('body');
 
 burger.addEventListener('click', () => {
-  return (burger.classList.toggle('active'),
+  return (
+    burger.classList.toggle('active'),
     menu.classList.toggle('active'),
     headerLine.classList.toggle('active'),
-    body.classList.toggle('lock'));
+    body.classList.toggle('lock')
+  );
 });
-
-
-
-
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
-const {resolve, join} = require('path');
+const { resolve, join } = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: resolve(__dirname, 'build'),
+    path: resolve(__dirname, 'dist'),
     filename: '[name]-[fullhash].js',
-    clean: true
-
+    clean: true,
   },
   module: {
     rules: [
@@ -16,41 +16,40 @@ module.exports = {
         test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env'],
-            plugins: ["@babel/plugin-transform-runtime"]
-          }
-        }
+            plugins: ['@babel/plugin-transform-runtime'],
+          },
+        },
       },
       {
         test: /\.s[ac]ss$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "sass-loader",
-        ],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
-    ]
+    ],
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: './src/index.html',
     }),
 
     new MiniCssExtractPlugin({
-      filename: '[name]-[fullhash].css'
-    })
+      filename: '[name]-[fullhash].css',
+    }),
+
+    new CopyPlugin({
+      patterns: [{ from: 'img', to: 'img' }],
+    }),
   ],
 
   devServer: {
     port: 8080,
     static: {
-      directory: join(__dirname, 'src')
-    }
+      directory: join(__dirname, 'dist'),
+    },
   },
 
   devtool: 'inline-source-map',
 };
-
